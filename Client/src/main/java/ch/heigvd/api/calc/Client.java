4dc5248183c3eadcf18355c26447f5d43d2f2ce0
Ylli Fazlija <fazlijaylli@gmail.com>
2022-11-09 15:00:09 +0100

    Added functionning client

--- Client/src/main/java/ch/heigvd/api/calc/Client.java
+++ Client/src/main/java/ch/heigvd/api/calc/Client.java
@@ -1,7 +1,7 @@
 package ch.heigvd.api.calc;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,11 +18,44 @@ public class Client {
      * @param args no args required
      */
     public static void main(String[] args) {
+
+        Socket clientSocket = null;
+        BufferedWriter out = null;
+        BufferedReader in = null;
+
         // Log output on a single line
         System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
 
-        BufferedReader stdin = null;
+        try {
+            clientSocket = new Socket("10.191.16.249", 12345);
+            out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
+            // Wait for welcome message
+            LOG.log(Level.INFO,"ATTENTE DU MESSAGE DU SERVEUR");
+            String line;
+            while ((line = in.readLine()) != null) {
+                LOG.log(Level.INFO, line);
+            }
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, ex.toString(), ex);
+        } finally {
+            try {
+                if (out != null) out.close();
+            } catch (IOException ex) {
+                LOG.log(Level.SEVERE, ex.toString(), ex);
+            }
+            try {
+                if (in != null) in.close();
+            } catch (IOException ex) {
+                LOG.log(Level.SEVERE, ex.toString(), ex);
+            }
+            try {
+                if (clientSocket != null && !clientSocket.isClosed()) clientSocket.close();
+            } catch (IOException ex) {
+                LOG.log(Level.SEVERE, ex.toString(), ex);
+            }
+        }
         /* TODO: Implement the client here, according to your specification
          *   The client has to do the following:
          *   - connect to the server
@@ -33,7 +66,7 @@ public class Client {
          *     - read the response line from the server (using BufferedReader.readLine)
          */
 
-        stdin = new BufferedReader(new InputStreamReader(System.in));
+        in = new BufferedReader(new InputStreamReader(System.in));
 
     }
 }
